af7108a345cb7a691e21aab69bfaf64cc7aa72ea
Mark Kolodziej <m2kolodz@uwaterloo.ca>
2023-11-01 18:48:18 -0400

    Remove confusing comments.  Done all header fields

--- webserver.py
+++ webserver.py
@@ -6,20 +6,18 @@ from datetime import datetime
 def getLastModified(fileName):
     timeModified = os.path.getmtime(fileName)
     timeZoned = time.asctime(time.gmtime(timeModified))
-    # print(timeModified)
-    # print(timeZoned)
-    # print(timeZoned.split())
+
     RFCFormat = timeZoned.split()[0] + ', ' + timeZoned.split()[2] + " " + timeZoned.split()[1] + " " + timeZoned.split()[4] + " " + timeZoned.split()[3] + " GMT"
-    # print(RFCFormat)
+   
     return RFCFormat
 
 def getDate():
-    # print("Curr gmt time")
+    
     currGmt = time.gmtime()
-    # print(currGmt)
+    
     timeZoned = time.asctime(currGmt)
     RFCFormat = timeZoned.split()[0] + ', ' + timeZoned.split()[2] + " " + timeZoned.split()[1] + " " + timeZoned.split()[4] + " " + timeZoned.split()[3] + " GMT"
-    # print(RFCFormat)
+    
     return RFCFormat
 
 
@@ -34,48 +32,71 @@ print("The server is ready to receive")
 while True:
     connectionSocket, addr = serverSocket.accept()
     request = connectionSocket.recv(2048).decode()
-    # print(type(request))
+
     fileName = request.partition('\n')[0].split()[1][1:]
-    # print(fileName)
+    
     response = ""
-    if fileName != "HelloWorld.html":
-        # 404
-        response = "HTTP/1.1 404 Not Found\n\n File Not Found"
-        print("File not found") # swap out for a 404 error msg
-        connectionSocket.sendall(response.encode())
-    else:
-        print("sending file")
+    statusMsg = ""
+    connection = ""
+    date = ""
+    server = ""
+    lastModified = ""
+    contentLength = ""
+    contentType = ""
+
+    
+    try:
         file = open(fileName, 'r')
+        print("Starting to send file")
         fileData = file.read()
         file.close()
-        # create headers
-        # Connection 14.10?
+
+        # Connection 14.10
         # Date 14.18
         # Server 14.38
         # Last-Modified 14.29
         # Content-Length 14.13
         # Content-Type 14.17
+        isHead = False
+        requestType = request.partition('\n')[0].split()[0]
+        if requestType == 'HEAD':
+            isHead = True
+
         statusMsg = "HTTP/1.1 200 OK\r\n"
         connection = "Connection: keep-alive\r\n"
-        date ="Date: " + str(getDate()) + "\r\n" # Look at the RFC Format, the actual values needs to be preceded with name like:
-        # Date: Tue, 15 Nov...
+        date ="Date: " + str(getDate()) + "\r\n" 
         server = "Server: Lab 2 Task 1; Python server\r\n"
         lastModified = "Last-Modified: " + str(getLastModified(fileName)) + "\r\n"
         
         contentLength = "Content-Length: " + str(len(fileData)) + "\r\n"
         contentType = "Content-Type: text/html\r\n\r\n"
-        # headers =  + contentLength
-        response = statusMsg + connection + date + server + lastModified + contentLength + contentType + fileData
+
+        response = statusMsg + connection + date + server + lastModified
+
+        if isHead:
+            contentLength = "Content-Length: " + str(0) + "\r\n"
+        response = response + contentLength + contentType
+        if not isHead:
+            response = response + fileData
         connectionSocket.sendall(response.encode())
-        # connectionSocket.sendall(contentLength.encode())
-        # connectionSocket.sendall(fileData.encode())
-        # send response
         print("Done sending file")
-    
+    except FileNotFoundError:
+        # 404
+        statusMsg = "HTTP/1.1 404 Not Found\r\n"
+        connection = "Connection: keep-alive\r\n"
+        date ="Date: " + str(getDate()) + "\r\n" 
+        server = "Server: Lab 2 Task 1; Python server\r\n"
+        lastModified = "Last-Modified: " + "Not Applicable" + "\r\n"
         
-    # sentence = connectionSocket.recv(2048).decode() # buffer size = 2048
-    # capitalizedSentence = sentence.upper()
-    # connectionSocket.send(capitalizedSentence.encode())
+        contentLength = "Content-Length: " + str(0) + "\r\n"
+        contentType = "Content-Type: Not Applicable\r\n\r\n"
+        
+        response = statusMsg + connection + date + server + lastModified
+        
+        response = response + contentLength + contentType
+        
+        connectionSocket.sendall(response.encode())
+        
+    
     connectionSocket.close()
-    print('.')
 
