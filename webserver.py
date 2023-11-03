466bfe33878f9dd8c4b95843896fd254f676ae8b
Mark Kolodziej <m2kolodz@uwaterloo.ca>
2023-11-02 21:31:45 -0400

    cleaning up comments.  Add ReadMe

--- webserver.py
+++ webserver.py
@@ -3,18 +3,16 @@ import os
 import time
 from datetime import datetime
 
+# helper function for Last-Modified field in response header
 def getLastModified(fileName):
     timeModified = os.path.getmtime(fileName)
     timeZoned = time.asctime(time.gmtime(timeModified))
-
     RFCFormat = timeZoned.split()[0] + ', ' + timeZoned.split()[2] + " " + timeZoned.split()[1] + " " + timeZoned.split()[4] + " " + timeZoned.split()[3] + " GMT"
    
     return RFCFormat
-
+# helper function for Date field in response header
 def getDate():
-    
     currGmt = time.gmtime()
-    
     timeZoned = time.asctime(currGmt)
     RFCFormat = timeZoned.split()[0] + ', ' + timeZoned.split()[2] + " " + timeZoned.split()[1] + " " + timeZoned.split()[4] + " " + timeZoned.split()[3] + " GMT"
     
@@ -33,8 +31,8 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     request = connectionSocket.recv(2048).decode()
 
+
     fileName = request.partition('\n')[0].split()[1][1:]
-    
     response = ""
     statusMsg = ""
     connection = ""
@@ -43,20 +41,13 @@ while True:
     lastModified = ""
     contentLength = ""
     contentType = ""
-
     
+    # Try-Except to find requested file.  If found then file data used in response.  If not, 404 response returned
     try:
         file = open(fileName, 'r')
         print("Starting to send file")
         fileData = file.read()
         file.close()
-
-        # Connection 14.10
-        # Date 14.18
-        # Server 14.38
-        # Last-Modified 14.29
-        # Content-Length 14.13
-        # Content-Type 14.17
         isHead = False
         requestType = request.partition('\n')[0].split()[0]
         if requestType == 'HEAD':
@@ -67,7 +58,6 @@ while True:
         date ="Date: " + str(getDate()) + "\r\n" 
         server = "Server: Lab 2 Task 1; Python server\r\n"
         lastModified = "Last-Modified: " + str(getLastModified(fileName)) + "\r\n"
-        
         contentLength = "Content-Length: " + str(len(fileData)) + "\r\n"
         contentType = "Content-Type: text/html\r\n\r\n"
 
@@ -82,19 +72,19 @@ while True:
         print("Done sending file")
     except FileNotFoundError:
         # 404
+        responseBody = "404 Not Found"
+        # print(len(responseBody))
         statusMsg = "HTTP/1.1 404 Not Found\r\n"
         connection = "Connection: keep-alive\r\n"
         date ="Date: " + str(getDate()) + "\r\n" 
         server = "Server: Lab 2 Task 1; Python server\r\n"
         lastModified = "Last-Modified: " + "Not Applicable" + "\r\n"
-        
-        contentLength = "Content-Length: " + str(0) + "\r\n"
-        contentType = "Content-Type: Not Applicable\r\n\r\n"
+        contentLength = "Content-Length: " + str(len(responseBody)+1) + "\r\n"
+        contentType = "Content-Type: text/html\r\n\r\n\n"
         
         response = statusMsg + connection + date + server + lastModified
-        
         response = response + contentLength + contentType
-        
+        response = response + responseBody
         connectionSocket.sendall(response.encode())
         
     
